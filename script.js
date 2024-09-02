66daee2ecbf7f471af1887301b56309105e4ac7d
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-02 16:14:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
+	let table = document.getElementById('sampleTable');
+	let newRow = table.insertRow(0);
+	let newCell1 = table.insertCell(0);
+	newCell1.innerHTML = 'New Cell';
+	let newCell2 = table.insertCell(1);
+	newCell2.innnerHTML = 'New Cell2';
   
 }
